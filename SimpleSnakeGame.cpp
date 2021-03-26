97750faf464383e8c66daf8d6c2c9ff0026d9f43
Divyanshu Singh <divic309>
2021-03-26 19:54:14 +0530

    Second Commit

--- SimpleSnakeGame.cpp
+++ SimpleSnakeGame.cpp
@@ -1,6 +1,59 @@
 #include<iostream>
 using namespace std;
-int main() {
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOwN};
+eDirection dir;
+void Setup() {
+     gameOver = false;
+     dir = STOP;
+     x = width / 2;
+     y = height / 2;
+     fruitX = rand() % width;
+     fruitY = rand() % height;
+     score = 0;
+}
 
-return 0;
+void Draw () {
+     system("cls");    //system ("clear");
+     for (int i = 0; i < width; i++)
+        cout<<"#";
+     cout<<endl;
+
+     for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            if (j == 0 )
+                cout<<"#";
+            else
+                cout<<" ";
+
+            if (j == width-1) {
+                cout<<"#";
+            }
+            cout<<endl;
+        }
+     }
+     for (int i = 0; i < width; i++)
+        cout<<"#";
+     cout<<endl;
+}
+
+void Input () {
+
+}
+
+void Logic () {
+
+}
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();\
+        //sleep(10);
+    }
+    return 0;
 }
